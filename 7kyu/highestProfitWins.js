2ed5e674498f0a391036e98edf7975ebda51297d
unknown <johnzak80@gmail.com>
2022-06-23 19:40:47 -0400

    update

--- 7kyu/highestProfitWins.js
+++ 7kyu/highestProfitWins.js
@@ -1,3 +1,7 @@
 function minMax(arr){
     arr.sort((a,b) => a-b)
     return Array(arr[0], arr[arr.length-1])
+
+    //  let maxMinArr = [arr.push(Math.max(arr), Math.min(arr))]
+//  return maxMinArr
+}
