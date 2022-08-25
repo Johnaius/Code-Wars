75e43bd694c4fa67b6f2de8b57fede4078fe9051
unknown <johnzak80@gmail.com>
2022-08-25 16:45:46 -0400

    fakeBinary

--- fakeBinary.js
+++ fakeBinary.js
@@ -1,3 +1,23 @@
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+function fakeBin(x){
+  let str = ''
+  console.log(x)
+  for(let i = 0; i < x.length ; i++){
+    if (x[i] < 5){
+      str += 0
+      
+    }else if(x[i] >= 5) {
+      str += 1  
+    
+      }
+        }
+          return str
+  }
+
 function fakeBin(x){
     let str = ''
     console.log(x)
