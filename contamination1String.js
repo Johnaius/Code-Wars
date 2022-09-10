f8fe3b3996594e9344aeea913f4322c7878351d6
unknown <johnzak80@gmail.com>
2022-09-10 19:53:41 -0400

    contam

---
+++ contamination1String.js
@@ -0,0 +1,12 @@
+function contamination(text, char){
+    return char.repeat(text.length)
+  }
+  
+  // Parameters: "text" is a string of anylength made of characters
+  // "char" i sa string of length 1
+  
+  // Results:  string text.length in length made up of char
+  
+  // Example: see description
+  
+  // Pseudocode:
