e0a09cffee00971788eec08038b3f1d7a9169565
unknown <johnzak80@gmail.com>
2022-10-22 13:53:51 -0400

    returnADay

---
+++ returnTheDay.js
@@ -0,0 +1,17 @@
+// Complete the function which returns the weekday according to the input number:
+
+// 1 returns "Sunday"
+// 2 returns "Monday"
+// 3 returns "Tuesday"
+// 4 returns "Wednesday"
+// 5 returns "Thursday"
+// 6 returns "Friday"
+// 7 returns "Saturday"
+// Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+function whatday(num) { 
+    //enter a number 1-7 no funny bz
+    //return corresponding say of week 1 ==="Sunday" 7 ==="Saturday"
+    // whatday(2), "Monday"
+    // whatday(7), "Saturday"
+    // whatday(88), "Wrong, please enter a number between 1 and 7"
