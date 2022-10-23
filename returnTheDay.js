0c83d68d97f5dc7750b1cfe9a11a672e1f28e219
unknown <johnzak80@gmail.com>
2022-10-23 13:54:20 -0400

    returnADay

--- returnTheDay.js
+++ returnTheDay.js
@@ -9,9 +9,28 @@
 // 7 returns "Saturday"
 // Otherwise returns "Wrong, please enter a number between 1 and 7"
 
-function whatday(num) { 
-    //enter a number 1-7 no funny bz
-    //return corresponding say of week 1 ==="Sunday" 7 ==="Saturday"
-    // whatday(2), "Monday"
-    // whatday(7), "Saturday"
-    // whatday(88), "Wrong, please enter a number between 1 and 7"
+
+    function whatday(num) { 
+        //enter a number 1-7 no funny bz
+        //return corresponding say of week 1 ==="Sunday" 7 ==="Saturday"
+        // whatday(2), "Monday"
+        // whatday(7), "Saturday"
+        // whatday(88), "Wrong, please enter a number between 1 and 7"
+          if(num === 1){
+            return "Sunday"
+          }else if(num == 2){
+            return "Monday"
+          }else if(num == 3){
+            return "Tuesday"
+          }else if(num == 4){
+            return "Wednesday"
+          }else if(num == 5){
+            return "Thursday"
+          }else if(num == 6){
+            return "Friday"
+          }else if(num == 7){
+            return "Saturday"
+          }else{
+            return "Wrong, please enter a number between 1 and 7"
+            }
+              }
