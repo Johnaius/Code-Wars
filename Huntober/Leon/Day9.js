cace5f458a5d6fec338882218ec68a97d51d49c9
John Zakrzewski <johnzak80@gmail.com>
2022-11-03 15:39:23 -0400

    tranportation

--- Huntober/Leon/Day9.js
+++ Huntober/Leon/Day9.js
@@ -10,6 +10,15 @@
     function last(str){
         str.split(' ').sort((a,b)=> a.slice(-1) > b.slice(-1))
     }
+              
+
+
+    
+
+    function last(str){
+       return str.split(' ').sort((a,b) => a[a.length-1] > b[b.length-1])
+    }
+
     console.log(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up'])
     console.log(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what'])
-    console.log(last('take me to semynak'), ['take', 'me', 'semynak', 'to'] )               
+    console.log(last('take me to semynak'), ['take', 'me', 'semynak', 'to'] )  
