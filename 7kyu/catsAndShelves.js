68f1d8f632fe4a7b9d793d4af188d24578ef4f0b
John Zakrzewski <johnzak80@gmail.com>
2022-11-13 19:55:50 -0500

    every array an array 7kyu

--- 7kyu/catsAndShelves.js
+++ 7kyu/catsAndShelves.js
@@ -1,6 +1,6 @@
 
-An infinite number of shelves are arranged one above the other in a staggered fashion.
-The cat can jump up to 3 shelves at the same time: from shelf 1 to shelf 2 or 4 (the cat cannot climb on the shelf directly above its head), according to the illustration:
+// An infinite number of shelves are arranged one above the other in a staggered fashion.
+// The cat can jump up to 3 shelves at the same time: from shelf 1 to shelf 2 or 4 (the cat cannot climb on the shelf directly above its head), according to the illustration:
 
 //                  ┌────────┐
 //                  │-6------│
---
+++ 7kyu/isEveryValueInTheArrayAnArray.js
@@ -0,0 +1,20 @@
+// Is every value in the array an array?
+
+// This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
+
+// Examples:
+
+// [[1],[2]] => true
+// ['1','2'] => false
+// [{1:1},{2:2}] => false
+
+
+const arrCheck = value =>{
+    let arr = []
+    value.forEach((ele, i) => {
+      if(Array.isArray(value[i]) === true){
+        arr.push(value[i])
+      }
+    })
+    return value.length === arr.length
+    }
