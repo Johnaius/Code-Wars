fc5c493bc70215ec12c354e4bf71db92537bfb63
John Zakrzewski <johnzak80@gmail.com>
2023-04-30 20:49:03 -0400

    assessment

--- nextChapter/Studies/Assessment.js
+++ nextChapter/Studies/Assessment.js
@@ -14,4 +14,33 @@ function multiply(a) {
   
   console.log(result); // Output: 120
 
+  //Inventory List
+  // Function to create an inventory list
+function inventoryList() {
+    let items = []; // Array to store the inventory items
   
+    // Function to add an item to the inventory
+    function add(name) {
+      let names = items.filter(e => e.name === name); // Check if an item with the same name already exists
+      if (names.length === 0) {
+        // If no item with the same name exists, add the item to the inventory
+        items.push({ name: name });
+      }
+    }
+  
+    // Function to remove an item from the inventory
+    function remove(name) {
+      // Filter the items array to exclude the item with the specified name
+      items = items.filter(e => e.name !== name);
+    }
+  
+    // Function to retrieve the list of item names in the inventory
+    function getList() {
+      // Map the items array to extract only the name property of each item
+      return items.map(e => e.name);
+    }
+  
+    // Return an object that exposes the add, remove, and getList methods
+    return { add, remove, getList };
+  }
+
